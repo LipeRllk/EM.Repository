d94bf85975398540bc2426141feb775dca27ebb6
Lipe <84376560+LipeRllk@users.noreply.github.com>
2025-09-06 13:18:16 -0300

    .

--- EM.Montador/obj/Debug/net9.0/EM.Montador.AssemblyInfo.cs
+++ EM.Montador/obj/Debug/net9.0/EM.Montador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EM.Montador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5d58069c5c1db2f9dc28cbc4f27ae8d3d593e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172e9624e5afc3cdf42f35d1e6b076ae65198201")]
 [assembly: System.Reflection.AssemblyProductAttribute("EM.Montador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EM.Montador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
